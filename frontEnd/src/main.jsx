0b151a4c24e5a2ed25439f35122e21ba1d2d92fe
rigby200602 <tungle3456@gmail.com>
2025-06-03 16:30:55 +0700

    use AppContextProvider

--- frontEnd/src/main.jsx
+++ frontEnd/src/main.jsx
@@ -3,12 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import { AppContextProvider } from './context/AppContext.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     {/* set up router */}
     <BrowserRouter>
-      <App />
+      <AppContextProvider>
+        <App />
+      </AppContextProvider>
     </BrowserRouter>
   </StrictMode>,
 )
