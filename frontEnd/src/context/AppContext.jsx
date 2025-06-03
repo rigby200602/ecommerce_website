bed014e9d5755cd0fe2b5ca97a3607515a3f4339
rigby200602 <tungle3456@gmail.com>
2025-06-03 16:29:56 +0700

    update AppContext

--- frontEnd/src/context/AppContext.jsx
+++ frontEnd/src/context/AppContext.jsx
@@ -1,3 +1,20 @@
-import { createContext } from "react";
+import { createContext, use, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const AppContext = createContext();
+
+export const AppContextProvider = ({ children }) => {
+
+    const navigate = useNavigate();
+    const [user, setUser] = useState(null);
+    const [isSeller, setIsSeller] = useState(false);
+
+    const value = { navigate, user, setUser, isSeller, setIsSeller }
+    return <AppContextProvider value={value}>
+        {children}
+    </AppContextProvider>
+}
+
+export const useAppContext = () => {
+    return useAppContext(AppContext)
+}
