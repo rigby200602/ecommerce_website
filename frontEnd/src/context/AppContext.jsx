a2d3c6cfa522cc69e201c3b1a94e442c5e177fb9
rigby200602 <tungle3456@gmail.com>
2025-06-04 16:33:26 +0700

    fix navbar

--- frontEnd/src/context/AppContext.jsx
+++ frontEnd/src/context/AppContext.jsx
@@ -8,13 +8,12 @@ export const AppContextProvider = ({ children }) => {
     const navigate = useNavigate();
     const [user, setUser] = useState(null);
     const [isSeller, setIsSeller] = useState(false);
+    const [showUserLogin, setShowUserLogin] = useState(false);
 
-    const value = { navigate, user, setUser, isSeller, setIsSeller }
+    const value = { navigate, user, setUser, isSeller, setIsSeller,
+        showUserLogin, setShowUserLogin
+     }
     return <AppContext.Provider value={value}>
         {children}
     </AppContext.Provider>
-}
-
-export const useAppContext = () => {
-    return useAppContext(AppContext)
 }
