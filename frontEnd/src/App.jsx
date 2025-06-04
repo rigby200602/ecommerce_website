fa8b2a1458d4cefeb7c299012b03aa9436546491
rigby200602 <tungle3456@gmail.com>
2025-06-04 20:00:28 +0700

    set up routing for banner

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
 
 const App = () => {
   return (
     <div>
       <Navbar/>
+      <div>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+        </Routes>
+      </div>
     </div>
   )
 }
