efecadf60206ab929f30e3515c7d187ecc1dd37d
rigby200602 <tungle3456@gmail.com>
2025-06-05 10:12:40 +0700

    create categories and add to home page

---
+++ frontEnd/src/components/Categories.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Categories = () => {
+  return (
+    <div>
+      <p>Catogories</p>
+      <div>
+        <div>
+            <img src={assets.box_icon} alt=''></img>
+            <p>Fruit</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Categories
