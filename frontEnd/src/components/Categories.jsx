c0cd659efb85931e14e43402a6dc48add2d392be
rigby200602 <tungle3456@gmail.com>
2025-06-05 10:19:04 +0700

    add style

--- frontEnd/src/components/Categories.jsx
+++ frontEnd/src/components/Categories.jsx
@@ -3,13 +3,17 @@ import { assets } from '../assets/assets'
 
 const Categories = () => {
   return (
-    <div>
-      <p>Catogories</p>
-      <div>
-        <div>
-            <img src={assets.box_icon} alt=''></img>
-            <p>Fruit</p>
+    <div className='mt-16'>
+      <p className='text-2xl md:text-3xl font-medium'>Catogories</p>
+      <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 lg:grid-cols-6
+      xl:grid-cols-7 mt-6 gap-6'>
+
+        <div className='group cursor-pointer py-5 px-3 gap-2 rounded-lg flex flex-col
+        justify-center items-center'>
+            <img className='group-hover:scale-108 transition max-w-28' src={assets.box_icon} alt=''></img>
+            <p className='text-sm font-medium'>Fruit</p>
         </div>
+
       </div>
     </div>
   )
