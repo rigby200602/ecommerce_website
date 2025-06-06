ca6436cac62d7b61164cf31675694e61d7b8bfed
rigby200602 <tungle3456@gmail.com>
2025-06-06 16:03:58 +0700

    add product api

--- frontEnd/src/context/AppContext.jsx
+++ frontEnd/src/context/AppContext.jsx
@@ -1,27 +1,44 @@
 import { createContext, use, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { dummyProducts } from "../assets/assets";
+import toast from "react-hot-toast";
 
 export const AppContext = createContext();
 
 export const AppContextProvider = ({ children }) => {
+
+    const currency = import.meta.VITE_CURRENCY;
 
     const navigate = useNavigate();
     const [user, setUser] = useState(false);
     const [isSeller, setIsSeller] = useState(false);
     const [showUserLogin, setShowUserLogin] = useState(false);
     const [product, setProduct] = useState([])
-
+    const [cartItems, setCartItems] = useState({})
+// Fetch All Products
     const fetchProduct = async () => {
         setProduct(dummyProducts)
     }
+// Add Product to Cart 
+    const addToCart = () =>{
+        let cartData = structuredClone(cartItems);
+
+        if (cartData[itemId]){
+            cartData[itemId] += 1;
+        } else {
+            cartData[itemId] = 1;
+        }
+        setCartItems(cartData);
+        toast.success("Added to Cart")
+    }
+
 
     useEffect(()=>{
         fetchProduct()
     },[])
 
     const value = { navigate, user, setUser, isSeller, setIsSeller,
-        showUserLogin, setShowUserLogin, product, setProduct
+        showUserLogin, setShowUserLogin, product, currency
      }
     return <AppContext.Provider value={value}>
         {children}
