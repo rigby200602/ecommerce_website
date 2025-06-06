c040c5796ab0faafa1359a99d743e554f4a1c8f4
rigby200602 <tungle3456@gmail.com>
2025-06-06 16:21:27 +0700

    fix ProductCard

--- frontEnd/src/components/BestSeller.jsx
+++ frontEnd/src/components/BestSeller.jsx
@@ -1,11 +1,17 @@
+import { useContext } from 'react'
 import ProductCard from './ProductCard'
+import { AppContext } from '../context/AppContext'
 
 const BestSeller = () => {
+  
+  const products = useContext(AppContext)
+
   return (
     <div className='mt-16'>
         <p className='text-2xl md:text-3xl font-medium'>Best Seller</p>
         <div>
-          <ProductCard/>
+          {/* Show 1st product */}
+          <ProductCard product={products[0]}/>
         </div>
     </div>
   )
