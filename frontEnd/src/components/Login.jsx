4ebcf5209e96e0ce85583ef04a2d3fbf0edc760a
rigby200602 <tungle3456@gmail.com>
2025-06-09 22:20:01 +0700

    fix

--- frontEnd/src/components/Login.jsx
+++ frontEnd/src/components/Login.jsx
@@ -16,12 +16,13 @@ const Login = () => {
             name: "test"
         })
         setShowUserLogin(false)
+        console(email,name)
     }
 
     return (
         <div onClick={() => setShowUserLogin(false)}className='fixed top-0 bottom-0 left-0 right-0 z-30 flex items-center
         text-sm text-gray-600 bg-black/50'>
-            <form onSubmit={onSubmitHandler()}
+            <form onSubmit={onSubmitHandler}
             onClick={(e) => e.stopPropagation()}
             className="flex flex-col gap-4 m-auto items-start p-8 py-12 w-80 sm:w-[352px] rounded-lg shadow-xl border border-gray-200 bg-white">
                 <p className="text-2xl font-medium m-auto">
