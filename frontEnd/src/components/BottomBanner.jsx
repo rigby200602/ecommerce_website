8363c564cc020a20a109600b18db119240538402
rigby200602 <tungle3456@gmail.com>
2025-06-09 15:59:36 +0700

    done BottomBanner

--- frontEnd/src/components/BottomBanner.jsx
+++ frontEnd/src/components/BottomBanner.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
-import { assets } from '../assets/assets'
+import { assets, features } from '../assets/assets'
 
 const BottomBanner = () => {
     return (
         <div className='relative mt-24'>
             <img className="w-full hidden md:block" src={assets.bottom_banner_image} alt="bottom_banner" />
             <img className='w-full block md:hidden' src={assets.bottom_banner_image_sm} alt="bottom_banner" />
+            {/* Show features in banner */}
+            <div className='absolute inset-0 flex flex-col items-center md:items-end md:justify-center
+            pt-16 md:pt-0 md:pr-24'>
+                <div>
+                    <h1 className='text-2xl md:text-3xl font-semibold text-primary mb-6'>Why We Are The Best?</h1>
+                    {features.map((feature, i) => (
+                        <div className="flex items-center gap-4 mt-2"key={i}>
+                            <img className="md:w-11 w-9" src={feature.icon} alt={feature.title}/>
+                            <h3 className='text-lg md:text-xl font-semibold'>{feature.title}</h3>
+                            <p className='text-gray-500/70 text-xs md:text-sm'>{feature.description}</p>
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
     )
 }
