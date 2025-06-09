e0af027bb8208b00dde5b3a5ee7df3d65a50ac17
rigby200602 <tungle3456@gmail.com>
2025-06-09 20:36:10 +0700

    add newletter

--- frontEnd/src/pages/Home.jsx
+++ frontEnd/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import MainBanner from '../components/MainBanner'
 import Categories from '../components/Categories'
 import BestSeller from '../components/BestSeller'
 import BottomBanner from '../components/BottomBanner'
+import NewLetter from '../components/NewLetter'
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
       <Categories/>
       <BestSeller/>
       <BottomBanner/>
+      <NewLetter/>
     </div>
   )
 }
