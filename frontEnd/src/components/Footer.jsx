82462fa75db2a8fa8994f81d7ae4cc075b46efcf
rigby200602 <tungle3456@gmail.com>
2025-06-09 20:47:30 +0700

    fix text

--- frontEnd/src/components/Footer.jsx
+++ frontEnd/src/components/Footer.jsx
@@ -6,7 +6,7 @@ const Footer = () => {
             <div className="flex flex-col md:flex-row items-start justify-between gap-10 py-10 border-b border-gray-500/30 text-gray-500">
                 <div>
                     <img className="w-34 md:w-32" src={assets.logo} alt="logo" />
-                    <p className="max-w-[410px] mt-6">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum unde quaerat eveniet cumque accusamus atque qui error quo enim fugiat?</p>
+                    <p className="max-w-[410px] mt-6">We deliver fresh groceries and snacks straight to your door. Trusted by thousands, we aim to make your shopping experience simple and affordable.</p>
                 </div>
                 <div className="flex flex-wrap justify-between w-full md:w-[45%] gap-5">
                     {footerLinks.map((section, index) => (
