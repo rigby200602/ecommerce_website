7cde54bbcea3bd97ef7a246ca19438d899efa6e2
rigby200602 <tungle3456@gmail.com>
2025-06-09 21:05:48 +0700

    remove console log

--- frontEnd/src/components/Navbar.jsx
+++ frontEnd/src/components/Navbar.jsx
@@ -76,7 +76,6 @@ const Navbar = () => {
                         <button onClick={() => {
                             setOpen(false);
                             setShowUserLogin(true);
-                            console.log(showUserLogin);
                         }}
                             className="cursor-pointer px-6 py-2 mt-2 bg-indigo-500 hover:bg-indigo-600 transition text-white rounded-full text-sm">
                             Login
