a0e5a5710b017fb40565003919f5c75238b6149a
rigby200602 <tungle3456@gmail.com>
2025-06-10 21:20:50 +0700

    fix

--- frontEnd/src/pages/AllProducts.jsx
+++ frontEnd/src/pages/AllProducts.jsx
@@ -13,14 +13,13 @@ const AllProducts = () => {
       <div className='flex flex-col items-end w-max'>
         <p className='text-2xl font-medium uppercase'>All products</p>
         <div className='w-16 h-0.5 bg-primary rounded-full'></div>
-
-        {/* Product list */}
+      </div>
+      {/* Product list */}
         <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-2 md:gap-6'>
             {dummyProducts.map((product,i) => (
                 <ProductCard product={product} key={i}/>
             ))}
         </div>
-      </div>
     </div>
   )
 }
