7de0ee067e11d38df67e83af3dc0561c69df2d55
rigby200602 <tungle3456@gmail.com>
2025-06-11 12:49:21 +0700

    add ProductCategory page and mount to app

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -7,6 +7,7 @@ import Footer from './components/Footer'
 import { AppContext } from './context/AppContext'
 import Login from './components/Login'
 import AllProducts from './pages/AllProducts'
+import ProductCategory from './pages/ProductCategory'
 
 const App = () => {
   
@@ -23,6 +24,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home/>}/>
           <Route path='/products' element={<AllProducts/>}></Route>
+          <Route path='/products/:category' element={<ProductCategory/>}/>
         </Routes>
       </div>
       {!isSellerPath && <Footer/>}
