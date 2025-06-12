8b6a05875a761bca7bf316720dab2821ac264cc5
rigby200602 <tungle3456@gmail.com>
2025-06-12 15:49:52 +0700

    update show number item in cart

--- frontEnd/src/components/Navbar.jsx
+++ frontEnd/src/components/Navbar.jsx
@@ -7,7 +7,7 @@ import { AppContext } from '../context/AppContext'
 const Navbar = () => {
     const [open, setOpen] = useState(false)
     const { user, setUser, setShowUserLogin, navigate, showUserLogin,
-        searchQuery, setSearchQuery
+        searchQuery, setSearchQuery, getCartCount
     } = useContext(AppContext);
 
     const logout = async () => {
@@ -44,7 +44,7 @@ const Navbar = () => {
 
                 <div onClick={() => navigate("/cart")} className="relative cursor-pointer">
                     <img className='w-6 opacity-80' src={assets.nav_cart_icon} alt='cart' />
-                    <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">3</button>
+                    <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">{getCartCount()}</button>
                 </div>
                 {!user ?
                     (
