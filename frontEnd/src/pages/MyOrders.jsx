6025f09610736f9d5042afc50b2470529554837c
rigby200602 <tungle3456@gmail.com>
2025-07-08 21:20:36 +0700

    update css

--- frontEnd/src/pages/MyOrders.jsx
+++ frontEnd/src/pages/MyOrders.jsx
@@ -40,7 +40,7 @@ const MyOrders = () => {
                                 <p>Category: {item.product.category}</p>
                             </div>
                             </div>
-                            <div className='text-primary text-lg font-medium'>
+                            <div className='flex flex-col justify-center md:ml-8 mb-4 md:mb-0'>
                                 <p>Quantity: {item.quantity || "1"}</p>
                                 <p>Status: {item.status || "1"}</p>
                                 <p>Date: {new Date(order.createdAt).toLocaleDateString()}</p>
