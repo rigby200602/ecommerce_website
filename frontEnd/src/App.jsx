09bbc269e422f5226fe4e65cc4e1b636fc402cc6
rigby200602 <tungle3456@gmail.com>
2025-07-09 22:28:53 +0700

    done layout seller login form

--- frontEnd/src/App.jsx
+++ frontEnd/src/App.jsx
@@ -12,11 +12,12 @@ import ProductDetails from './pages/ProductDetails'
 import Cart from './pages/Cart'
 import AddAddress from './pages/AddAddress'
 import MyOrders from './pages/MyOrders'
+import SellerLogin from './components/seller/SellerLogin'
 
 const App = () => {
   
   const isSellerPath = useLocation().pathname.includes("seller");
-  const {showUserLogin} = useContext(AppContext)
+  const {showUserLogin, isSeller} = useContext(AppContext)
   return (
     <div>
       {isSellerPath ? null : <Navbar/> }  
@@ -33,6 +34,7 @@ const App = () => {
           <Route path='/cart' element={<Cart/>}/>
           <Route path='/add-address' element={<AddAddress/>}/>
           <Route path='/my-orders' element={<MyOrders/>} />
+          <Route path='/seller' element={isSeller ? null : <SellerLogin/>}></Route>
         </Routes>
       </div>
       {!isSellerPath && <Footer/>}
