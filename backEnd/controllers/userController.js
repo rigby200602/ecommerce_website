f95e851a8b1937748f8be5a1a246d62ac6a1131f
rigby200602 <tungle3456@gmail.com>
2025-08-14 22:31:41 +0700

    userSchema, userController

---
+++ backEnd/controllers/userController.js
@@ -0,0 +1,35 @@
+import User from "../models/user";
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken'
+
+// Register User: /api/user/register
+export const register = async (req,res) => {
+    try {
+        const { name, email, password } = req.body;
+
+        if (!name || !email|| !password) {
+            return res.json({success: false, message: 'Missing Details'})
+        }
+
+        const existingUser = await User.findOne({email})
+
+        if (existingUser) return res.json({success: false, message: 'User already exists'})
+        
+        const hasdedPassword = await bcrypt.hash(password, 10)
+
+        const user = await User.create({name, email, password: hasdedPassword})
+
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn: '7d'})
+
+        res.cookie('token', token, {
+            httpOnly: true, // Prevent JS to access cookie
+            secure: process.env.NODE_ENV === 'production', // Use secure cookie in production
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict', // CSRF protection
+            maxAge: 7 * 24 * 60 * 60 * 1000, // Cookie expiration time
+        })
+
+        return res.json({success: true, user: {email: user.email, name: user.name},message: 'User have beencreated'})
+    } catch (e) {
+        res.json({ success: false, massage: e.massage})
+    }
+}
