b9caa4a76a7b44df636285ab2bc787c6929d5b61
rigby200602 <tungle3456@gmail.com>
2025-08-17 21:15:16 +0700

    login api

--- backEnd/controllers/userController.js
+++ backEnd/controllers/userController.js
@@ -30,6 +30,40 @@ export const register = async (req,res) => {
 
         return res.json({success: true, user: {email: user.email, name: user.name},message: 'User have been created'})
     } catch (e) {
+        console.log(e.message)
+        res.json({ success: false, massage: e.massage})
+    }
+}
+
+// Login User: /api/user/login
+
+export const login = async (req,res) => {
+    try {
+        const {email, password} = req.body
+
+        if (!email || !password) return res.json({success: false, message: 'Email and password are required'})
+        const user = await User.findOne({email})
+        
+        if (!user) {
+           return res.json({success: false, message: 'Invalid email or password'})
+        }
+        const isMatch = await bcrypt.compare(password, user.password) 
+
+        if (!isMatch) return res.json({success: false, message: 'Invalid email or password'})
+
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn: '7d'})
+
+        res.cookie('token', token, {
+            httpOnly: true, // Prevent JS to access cookie
+            secure: process.env.NODE_ENV === 'production', // Use secure cookie in production
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict', // CSRF protection
+            maxAge: 7 * 24 * 60 * 60 * 1000, // Cookie expiration time
+        })
+
+        return res.json({success: true, user: {email: user.email, name: user.name}})
+    }
+    catch (e) {
+        console.log(e.message)
         res.json({ success: false, massage: e.massage})
     }
 }
