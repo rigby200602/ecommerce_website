5399fca11082fef342e34f32cae697524af734df
rigby200602 <tungle3456@gmail.com>
2025-08-17 22:09:48 +0700

    authSeller

---
+++ backEnd/middlewares/authSeller.js
@@ -0,0 +1,21 @@
+import jwt from 'jsonwebtoken'
+
+const authSeller = async (req,req,next) => {
+    const { sellerToken } = req.cookies
+
+    if (!sellerToken) {
+        return res.json({ success: false, message: 'Not Authorized'})
+    }
+    try {
+            const tokenDecode = jwt.verify(sellerToken, process.env.JWT_SECRET)
+            if (tokenDecode.email === process.env.SELLER_EMAIL) {
+                next()
+            } else {
+                return res.json({sucess: false, message: 'Not Authorized'})
+            }
+            next()
+        } catch (e) {
+            res.json({success:false, message: e.message})
+        }
+}
+export default authSeller
