eb8d16586f08834f4eec95597b3a7e5c1ada4c9f
rigby200602 <tungle3456@gmail.com>
2025-08-18 20:54:17 +0700

    set up cloudinary

--- backEnd/server.js
+++ backEnd/server.js
@@ -5,12 +5,14 @@ import connectDB from "./configs/db.js";
 import 'dotenv/config'
 import userRouter from "./routes/userRoute.js";
 import sellerRouter from "./routes/sellerRoute.js";
+import connectCloudinary from "./configs/cloudinary.js";
 
 const app = express()
 const port = process.env.Port || 4000
 
 // Connect database
 await connectDB()
+await connectCloudinary()
 
 // Allow multiple origins
 const allowedOrigins = ['http://localhost:5173']
