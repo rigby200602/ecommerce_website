c3960ed6328677a70990e10beb80f6b3139d4f69
rigby200602 <tungle3456@gmail.com>
2025-08-18 22:01:22 +0700

    update cart api

---
+++ backEnd/controllers/cartController.js
@@ -0,0 +1,15 @@
+import User from "../models/User.js"
+
+// Update User CartData: /api/cart/update
+
+
+export const updateCart = async (req, res) => {
+    try {
+        const { userId, cartItems } = req.body
+        await User.findByIdAndUpdate(userId, {cartItems})
+        res.json({ success: true, messagae: "Cart Updated"})
+    } catch (e) {
+        console.log(e.messagae)
+        res.json({success: false, messagae: e.messagae})
+    }
+}
